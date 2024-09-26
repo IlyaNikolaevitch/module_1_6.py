56c24db96d055534f83775e4a53168a0dc527af6
IlyaNikolaevitch <mister.bin1981@gmail.com>
2024-09-26 21:19:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+my_dict = {'Иван': 1990, 'Сергей': 1995, 'Ирина': 2000}
+print(my_dict)
+print(my_dict.get('Сергей'))
+my_dict['Николай'] = 1981
+print(my_dict)
+my_dict.update({'Петр': 2001, 'Наталья': 1985})
+del my_dict['Ирина']
+print(my_dict.get('Ирина'))
+print(my_dict)
+my_set = {1,2,3,4,1,4,2,2, 'Вишня', True, 'Слива', 'Вишня', True}
+print(my_set)
+my_set.update({10,'Яблоко'})
+my_set.remove(4)
+print(my_set)
+
+
