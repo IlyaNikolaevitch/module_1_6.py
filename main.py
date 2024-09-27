94188f359c7ef3065cd16385fbdbb82ee3a20a6d
IlyaNikolaevitch <mister.bin1981@gmail.com>
2024-09-27 20:39:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ print(my_dict.get('Сергей'))
 my_dict['Николай'] = 1981
 print(my_dict)
 my_dict.update({'Петр': 2001, 'Наталья': 1985})
-del my_dict['Ирина']
-print(my_dict.get('Ирина'))
+a = my_dict.pop('Ирина')
 print(my_dict)
+print(a)
 my_set = {1,2,3,4,1,4,2,2, 'Вишня', True, 'Слива', 'Вишня', True}
 print(my_set)
 my_set.update({10,'Яблоко'})
